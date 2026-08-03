c6185f81995da3664943d05c65c1b5cbf7575767
Mmiirrkk <cot@wp.pl>
2026-08-03 10:45:12 +0200

    fix enum 1

--- datamodels/projects_overview/projects_overview.py
+++ datamodels/projects_overview/projects_overview.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from enum import Enum
+from enum import StrEnum
 
 from pydantic import BaseModel, ConfigDict, Field
 
 
-class Status(str, Enum):
+class Status(StrEnum):
     ONGOING = "ongoing"
     PAUSED = "paused"
     HALTED = "halted"
